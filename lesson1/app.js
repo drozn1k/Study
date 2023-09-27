b3403882169de35a3845934f790afe0eade6192c
Mykyta <77362863+drozn1k@users.noreply.github.com>
2023-09-27 19:45:14 +0300

    First QA in console

---
+++ lesson1/app.js
@@ -0,0 +1,10 @@
+let answer1, answer2, answer3;
+
+answer1 = prompt("Чи подобається тобі програмуванння на JS?", "так/ні")
+answer2 = prompt("Чи знаєш ти що таке змінні та як їх використовувати?", "так/ні")
+answer3 = prompt("Чи хочеш ти дізнатись більше про JS?", "так/ні")
+
+console.log("Ваші відповіді: ");
+console.log("1. " + answer1);
+console.log("2. " + answer2);
+console.log("3. " + answer3);
